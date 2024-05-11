c01a32930fb1200890633893d32f94a93d2e356b
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-05-11 12:31:12 +0400

    (fix): provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
+      source = "integrations/github"
+      version = "6.2.1"
     }
   }
 }
 
-# Configure the GitHub Provider
 provider "github" {
   token = var.github_token
   owner = var.github_owner
