a7610e25fba3039c2cfea64d16c371d0e4775da6
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-05-11 12:11:30 +0400

    (fix): provider owner var

--- provider.tf
+++ provider.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 # Configure the GitHub Provider
-provider "github" {}
-
-# Add a user to the organization
-# resource "github_membership" "membership_for_user_x" {
-#   # ...
-# }
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
